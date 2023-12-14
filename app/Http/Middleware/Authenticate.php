6f18a3bf4845535dbf6bc45f24e8816169738bf8
Dylo <dylogic06@gmail.com>
2023-12-14 09:56:58 +0100

    comit

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : route('login');
+    }
+
+    // API
+    protected function unauthenticated(Request $request, array $guards) {
+        $baseController = new BaseController();
+        abort($baseController->sendError('Unauthorized', ['error' => 'Sikertelen bejelentkezés!'], 401));
+    }
+}
